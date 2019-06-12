631c6f4776a1c94941d7e3adb9034d1b8f80d2ca
OLUWATOBI ADEOYE <oluwatobiadeoye@OLUWATOBIs-MBP.modem>
2019-06-13 05:52:03 +1000

    version 1.1

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 module.exports = {
     init: () => {
         firebase.initializeApp({
-            apiKey: "AIzaSyDuiHk85CEXDXpi31DXN1-gIPjNeXPeqWg",
-            authDomain: "careapp-fe9dc.firebaseapp.com",
-            databaseURL: "https://careapp-fe9dc.firebaseio.com",
-            projectId: "careapp-fe9dc",
-            storageBucket: "careapp-fe9dc.appspot.com",
-            messagingSenderId: "944161958584",
-            appId: "1:944161958584:web:9e0f27015e499a12"
+            apiKey: "AIzaSyD8TMsR1PHpe1WONMXXlPAhr0VxP6Ed46o",
+            authDomain: "carezone-fd9c2.firebaseapp.com",
+            databaseURL: "https://carezone-fd9c2.firebaseio.com",
+            projectId: "carezone-fd9c2",
+            storageBucket: "carezone-fd9c2.appspot.com",
+            messagingSenderId: "1091716940899",
+            appId: "1:1091716940899:web:2439d106f30f0d8f"
         })
     }
 }
